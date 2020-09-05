a6f6c4ad7218278d77b203b1e3403c1557ed62a4
AP <porwal.1@iitj.ac.in>
2020-09-05 13:53:51 +0530

    questions added

--- command/execute.js
+++ command/execute.js
@@ -1,25 +1,58 @@
 const program = require("commander");
 const { addcustomer, findcustomer } = require("../commands");
-const { find } = require("../config/commands");
+const { prompt } = require("inquirer");
+
+//Questions for adding Customer
+let questions = [
+    {
+        type: "input",
+        name: "firstname",
+        message: "Customer First Name"
+    },
+    {
+        type: "input",
+        name: "lastname",
+        message: "Customer Last Name"
+    }, {
+        type: "input",
+        name: "phone",
+        message: "Customer Phone Number"
+    }, {
+        type: "input",
+        name: "email",
+        message: "Customer Email"
+    }
+]
 
 program
     .version('1.0.0')
     .description('Client Management System');
 
 //Add a customer
+
+// program
+//     .command("add <firstname> <lastname> <phone> <email>")
+//     .alias('a')
+//     .description("Add a customer")
+//     .action((firstname, lastname, phone, email) => {
+//         let customer = {
+//             firstname: firstname,
+//             lastname: lastname,
+//             phone: phone,
+//             email: email
+//         }
+//         addcustomer(customer)
+//     })
+
 program
-    .command("add <firstname> <lastname> <phone> <email>")
-    .alias('a')
+    .command("add")
+    .alias("a")
     .description("Add a customer")
-    .action((firstname, lastname, phone, email) => {
-        let customer = {
-            firstname: firstname,
-            lastname: lastname,
-            phone: phone,
-            email: email
-        }
-        addcustomer(customer)
+    .action(() => {
+        prompt(questions).then(answers => addcustomer(answers))
     })
+
+
 
 program
     .command("find <name>")
